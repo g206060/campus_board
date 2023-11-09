4f0ad1ec2b03055bff83c3c6508ae7cbd1768fbe
g206060 <g206060@hi-tech.ac.jp>
2023-11-09 19:21:01 +0000

    掲示物詳細ページ完成

--- board/urls.py
+++ board/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'board'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('post-detail/<int:pk>/', views.PostDetailView.as_view(), name="post_detail"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,17 @@
 from django.views import generic
 
-class IndexView(generic.TemplateView):
+from .models import Board, Post
+
+class IndexView(generic.ListView):
+    model = Post
     template_name = 'index.html'
+    paginate_by = 3
+    
+class DetailView(generic.DetailView):
+    model = Post
+    slug_field = "post_title"
+    slug_url_kwarg = "post_title"
+    
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "post_detail.html"
